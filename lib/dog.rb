042b8265b8f5cd711c7c82324f75a70637164561
Mulu <brianmulu007@gmail.com>
2022-10-17 15:16:24 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # Instance method 1 (set)
+    # takes in argument of a dog's name 
+    # and assigns that value to a variable, this_dogs_name
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
 
+    # Instance method 2
+    # Reports the name (get)
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+# calls the Dog#name method with a value of "lassie"
+lassie.name = "Lassie"
+puts lassie.name
